bd00544b849bddd1937d4ab0024e6b7e40d05194
Oriol Blanc <oriolblanc@gmail.com>
2012-09-10 11:19:20 +0200

    Added OBTabBarController classes

---
+++ OBTabBarController.h
@@ -0,0 +1,11 @@
+//
+//  OBTabBarController.m
+//  Fever
+//
+//  Created by Oriol Blanc on 11/22/11.
+//  Copyright (c) 2011 Oriol Blanc. All rights reserved.
+//
+
+@interface OBTabBarController : UIViewController
+
+@end
---
+++ OBTabBarController.m
@@ -0,0 +1,13 @@
+//
+//  OBTabBarController.m
+//  Fever
+//
+//  Created by Oriol Blanc on 11/22/11.
+//  Copyright (c) 2011 Oriol Blanc. All rights reserved.
+//
+
+#import "OBTabBarController.h"
+
+@implementation OBTabBarController
+
+@end
