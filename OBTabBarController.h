11bf8896896b45edd5e35927fa7488ce73a0aad5
Oriol Blanc <oriolblanc@gmail.com>
2012-09-10 11:22:04 +0200

    Headers

--- OBTabBarController.h
+++ OBTabBarController.h
@@ -6,6 +6,19 @@
 //  Copyright (c) 2011 Oriol Blanc. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
 @interface OBTabBarController : UIViewController
+
+@property (nonatomic, retain) NSArray *viewControllers;
+@property (nonatomic, assign) NSInteger selectedIndex;
+
+- (id)initWithViewControllers:(NSArray *)viewControllers;
+
+- (void)presentModalViewController:(UIViewController *)modalViewController animated:(BOOL)animated;
+- (void)dismissModalViewControllerAnimated:(BOOL)animated;
+- (void)setTabBarHidden:(BOOL)hidden animated:(BOOL)animated;
+
++ (CGFloat)tabBarHeight;
 
 @end
